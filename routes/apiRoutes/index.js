b000d3682337217d53c52c57f4e01cbbc9722f78
Breania Harvey <harvey.breania@gmail.com>
2021-10-19 17:48:55 -0500

    server and routes functional

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes')
 
 // employing the router & having it use the module exported from animalRoutes.js
 router.use(animalRoutes);
 
-router.use(require('./zookeeperRoutes'));
+router.use(zookeeperRoutes);
 
 module.exports = router;
